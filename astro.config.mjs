ffa926ed0b29acdb33f45cf843b775b14121a8c7
Aldi Saputra <saputraaldi909090@gmail.com>
2026-04-06 16:13:03 +0700

    add sitemap and change wrong header

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,7 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-
 import tailwindcss from '@tailwindcss/vite';
-import sitemap from '@astrojs/sitemap';
+import sitemap from '@astrojs/sitemap'
 
 // https://astro.build/config
 export default defineConfig({
